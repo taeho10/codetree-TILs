64f9fc13982e71e4174b0407db65752ef1975971
taeho10 <145317777+taeho10@users.noreply.github.com>
2023-12-07 12:27:27 +0800

    Title: 2가 3번째로 등장하는 위치, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231207/2가 3번째로 등장하는 위치/where-2-appears-3rd.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include<stdlib.h>
+int main() {
+    int n;
+    int *p;
+    int i;
+    scanf("%d",&n);
+    p=(int *)malloc(sizeof(int)*n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&p[i]);
+        if(p[i]==2){
+            printf("%d",i=3);
+            break;
+        }
+    }
+    return 0;
+}
